06d5a34c972080f706faed2c3c221a606a5b0f58
parlamentifaklya <20.kokenyesi.andor@mechwart.com>
2024-10-09 11:47:38 +0200

    hatha mukod, ha nem akkor nemtudom

--- ViccAdatbazis/Pages/Index.cshtml.cs
+++ ViccAdatbazis/Pages/Index.cshtml.cs
@@ -1,20 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ViccAdatbazis.Data;
+using ViccAdatbazis.Models;
 
 namespace ViccAdatbazis.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly ViccDbContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, ViccDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
-        {
+        public List<Vicc> Viccek {  get; set; }
 
+        public async Task OnGetAsync()
+        {
+            Viccek = await _context.Viccek.Where(j => j.Aktiv).ToListAsync();
         }
     }
 }
